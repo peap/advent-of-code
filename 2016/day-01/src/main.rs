13c3ccecc0af3926af5c2083c743b5ff39813549
Eric Petersen <eric@huskers.unl.edu>
2016-12-01 22:04:45 -0800

    Committing part 1 of day 1 of 2016 AoC!

---
+++ 2016/day-01/src/main.rs
@@ -0,0 +1,137 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fmt;
+use std::fs::File;
+
+#[derive(Debug)]
+enum Heading {
+    East,
+    North,
+    West,
+    South,
+}
+
+#[derive(Debug)]
+struct Walker {
+    x: i32,  // + east, - west
+    y: i32,  // + north, - south
+    heading: Heading,
+}
+
+impl Walker {
+    fn new() -> Walker {
+        Walker { x:0, y:0, heading: Heading::East }
+    }
+
+    fn follow(&mut self, instructions: Vec<String>) {
+        for instr in instructions {
+            self.follow_instruction(&instr);
+        }
+    }
+
+    fn follow_instruction(&mut self, instruction: &str) {
+        let (direction, _distance) = instruction.trim().split_at(1);
+        self.heading = self.turn(direction);
+        let distance = _distance.parse::<i32>();
+        match distance {
+            Ok(num) => self.walk(num),
+            Err(_) => panic!("Count not parse distance, {}", _distance),
+        }
+    }
+
+    fn turn(&mut self, direction: &str) -> Heading {
+        match direction {
+            "L" => {
+                match self.heading {
+                    Heading::East => Heading::North,
+                    Heading::North => Heading::West,
+                    Heading::West => Heading::South,
+                    Heading::South => Heading::East,
+                }
+            }
+            "R" => {
+                match self.heading {
+                    Heading::East => Heading::South,
+                    Heading::North => Heading::East,
+                    Heading::West => Heading::North,
+                    Heading::South => Heading::West,
+                }
+            }
+            _ => panic!("Unknown direction: {}", direction),
+        }
+    }
+
+    fn walk(&mut self, distance: i32) {
+        match self.heading {
+            Heading::East => self.x += distance,
+            Heading::North => self.y += distance,
+            Heading::West => self.x -= distance,
+            Heading::South => self.y -= distance,
+        }
+    }
+
+    fn distance(&self) -> i32 {
+        self.x.abs() + self.y.abs()
+    }
+}
+
+impl fmt::Display for Walker {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Walker<{}, {}>", self.x, self.y)
+    }
+}
+
+
+fn parse_instructions(filename: &'static str) -> Vec<String> {
+    let file = File::open(filename).unwrap();
+    let mut reader = BufReader::new(file);
+    let mut line = String::new();
+    reader.read_line(&mut line).unwrap();
+    let instructions: Vec<String> = line.split(",").map(|x| x.to_string()).collect();
+    let num = instructions.len();
+    println!("Read {} instructions from input.txt", num);
+    instructions
+}
+
+fn main() {
+    let instructions = parse_instructions("input.txt");
+    let mut walker = Walker::new();
+    walker.follow(instructions);
+    println!("{} is {} blocks from the start", walker, &walker.distance());
+}
+
+#[test]
+fn easy_walk() {
+    let mut walker = Walker::new();
+    let instructions: Vec<String> = [
+        "R1",
+    ].iter().map(|x| x.to_string()).collect();
+    walker.follow(instructions);
+    assert_eq!(walker.distance(), 1);
+    assert_eq!((walker.x, walker.y), (0, -1));
+}
+
+#[test]
+fn less_easy_walk() {
+    let mut walker = Walker::new();
+    let instructions: Vec<String> = [
+        "R1",
+        "R10",
+    ].iter().map(|x| x.to_string()).collect();
+    walker.follow(instructions);
+    assert_eq!(walker.distance(), 11);
+    assert_eq!((walker.x, walker.y), (-10, -1));
+}
+
+#[test]
+fn longer_walker() {
+    let mut walker = Walker::new();
+    let instructions: Vec<String> = [
+        "R1",
+        "R10",
+        "L50",
+    ].iter().map(|x| x.to_string()).collect();
+    walker.follow(instructions);
+    assert_eq!(walker.distance(), 61);
+    assert_eq!((walker.x, walker.y), (-10, -51));
+}
