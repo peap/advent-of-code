3623f69a49f2b59a6c00bd66027ca3e01805fd92
Eric Petersen <eric@huskers.unl.edu>
2016-12-04 11:18:40 -0800

    add basic outline of day 4 problem

--- 2016/day-04/src/main.rs
+++ 2016/day-04/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+struct Room { }
+
+impl Room {
+    fn from_line(line: String) -> Room {
+        Room { }
+    }
+}
+
+fn load_rooms(filename: &'static str) -> Vec<Room> {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut rooms = Vec::new();
+    for line in reader.lines() {
+        rooms.push(Room::from_line(line.expect("Couldn't read a line.")))
+    }
+    rooms
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rooms = load_rooms("input.txt");
+    println!("Loaded {} rooms.", rooms.len());
 }
