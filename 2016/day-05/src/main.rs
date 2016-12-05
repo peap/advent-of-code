285725a024e28656649bfc7deb0ff084039984a0
Eric Petersen <eric@huskers.unl.edu>
2016-12-05 11:06:22 -0800

    Add Part 2. Run with --release!

--- 2016/day-05/src/main.rs
+++ 2016/day-05/src/main.rs
@@ -1,9 +1,11 @@
 extern crate crypto;
 
+use std::collections::HashSet;
+
 use crypto::md5::Md5;
 use crypto::digest::Digest;
 
-fn get_password(id: &str, length: usize) -> String {
+fn get_password_1(id: &str, length: usize) -> String {
     let key_base = id.as_bytes();
     let mut password_chars: Vec<char> = Vec::new();
     let mut hasher = Md5::new();
@@ -25,13 +27,54 @@ fn get_password(id: &str, length: usize) -> String {
     password_chars.into_iter().collect()
 }
 
+fn get_password_2(id: &str, length: usize) -> String {
+    let key_base = id.as_bytes();
+    let mut password_chars: Vec<char> = vec![0 as char; length];
+    let mut indices: HashSet<usize> = HashSet::new();
+    let mut hasher = Md5::new();
+    let mut hashed: [u8; 16] = [0; 16];
+    for i in 0..std::u64::MAX {
+        hasher.input(key_base);
+        hasher.input(i.to_string().as_bytes());
+        hasher.result(&mut hashed);
+        if hashed[0] as i32 + hashed[1] as i32 + (hashed[2] >> 4) as i32 == 0 {
+            let hashed_str = hasher.result_str();
+            let mut hex_chars = hashed_str.chars();  // .nth() requires mut
+            let index = hex_chars.nth(5).unwrap().to_string().parse::<usize>();
+            match index {
+                Ok(idx) => {
+                    if idx < length && !indices.contains(&idx) {
+                        // first 6 chars already consumed
+                        let chr = hex_chars.nth(0).unwrap();
+                        password_chars[idx] = chr;
+                        indices.insert(idx);
+                    }
+                }
+                _ => ()
+            }
+            if indices.len() == length {
+                break;
+            }
+        }
+        hasher.reset();
+    }
+    password_chars.into_iter().collect()
+}
+
 fn main() {
-    let id1 = "ffykfhsq";
-    let password1 = get_password(id1, 8);
-    println!("Part 1: {} --> {}", id1, password1);
+    let id = "ffykfhsq";
+    let password1 = get_password_1(id, 8);
+    println!("Part 1: {} --> {}", id, password1);
+    let password2 = get_password_2(id, 8);
+    println!("Part 2: {} --> {}", id, password2);
 }
 
 #[test]
-fn test_aoc_example() {
-    assert_eq!(get_password("abc", 8), "18f47a30");
+fn test_aoc_example_1() {
+    assert_eq!(get_password_1("abc", 8), "18f47a30");
+}
+
+#[test]
+fn test_aoc_example_2() {
+    assert_eq!(get_password_2("abc", 8), "05ace8e3");
 }
