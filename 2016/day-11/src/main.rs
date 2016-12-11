ea6965c4558e79c53b533d9cf49270111bd68269
Eric Petersen <eric@huskers.unl.edu>
2016-12-11 11:04:16 -0800

    Add some methods and correct initial building items

--- 2016/day-11/src/main.rs
+++ 2016/day-11/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Eq, PartialEq)]
 enum Isotope {
     Tm,  // Thulium
     Pu,  // Plutonium,
@@ -6,25 +7,66 @@ enum Isotope {
     Ru,  // Ruthenium,
 }
 
+#[derive(Debug, Eq, PartialEq)]
 enum Item {
     Generator(Isotope),
     Microchip(Isotope),
 }
 
+impl Item {
+    fn compatible_with(&self, other: &Item) -> bool {
+        use Item::*;
+        match self {
+            &Generator(ref iso) => {
+                match other {
+                    &Generator(_) => true,
+                    &Microchip(ref iso2) => iso == iso2,
+                }
+            }
+            &Microchip(ref iso) => {
+                match other {
+                    &Generator(ref iso2) => iso == iso2,
+                    &Microchip(_) => true,
+                }
+            }
+        }
+    }
+}
+
+struct Floor {
+    items: Vec<Item>,
+}
+
+impl Floor {
+    fn new(items: Vec<Item>) -> Floor {
+        Floor { items: items }
+    }
+}
+
 struct Building {
-    floors: Vec<Vec<Item>>,
+    floors: Vec<Floor>,
+}
+
+impl Building {
+    fn with_items(items: Vec<Vec<Item>>) -> Building {
+        let mut floors = Vec::new();
+        for floor_items in items {
+            floors.push(Floor::new(floor_items));
+        }
+        Building { floors: floors }
+    }
 }
 
 fn get_initial_building() -> Building {
     use Isotope::*;
     use Item::*;
-    let floors = vec![
+    // From input.txt:
+    Building::with_items(vec![
         vec![Generator(Tm), Microchip(Tm), Generator(Pu), Generator(Sr)],
         vec![Microchip(Pu), Microchip(Sr)],
-        vec![Generator(Pu), Microchip(Pu), Generator(Ru), Microchip(Ru)],
+        vec![Generator(Pm), Microchip(Pm), Generator(Ru), Microchip(Ru)],
         vec![],
-    ];
-    Building { floors: floors }
+    ])
 }
 
 
