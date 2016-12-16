040353ca60056daad797ceec8e68b31cfdcf3499
Eric Petersen <eric@huskers.unl.edu>
2016-12-15 20:30:34 -0800

    2015, Day 20, Part 2

--- 2015-rs/day-20/src/main.rs
+++ 2015-rs/day-20/src/main.rs
@@ -1,15 +1,16 @@
-pub const PART_1_NUM: u32 = 34_000_000;
+pub const TARGET: u32 = 34_000_000;
 
-const ELF_FACTOR: usize = 10;
-const LIMIT: usize = 1_000_000;
+const ELF_FACTOR1: usize = 10;
+const ELF_FACTOR2: usize = 11;
+const HOUSE_LIMIT: usize = 1_000_000;
 
-pub fn find_first_house_receiving(n_presents: u32) -> Option<u32> {
-    let mut houses: Vec<u32> = vec![0; LIMIT];
-    for elf in 1..LIMIT {
+pub fn find_first_house_receiving(target: u32) -> Option<u32> {
+    let mut houses: Vec<u32> = vec![0; HOUSE_LIMIT];
+    for elf in 1..HOUSE_LIMIT {
         let mut house = elf;
-        while house < LIMIT {
-            houses[house] += (elf * ELF_FACTOR) as u32;
-            if houses[house] >= n_presents {
+        while house < HOUSE_LIMIT {
+            houses[house] += (elf * ELF_FACTOR1) as u32;
+            if houses[house] >= target {
                 return Some(house as u32);
             }
             house += elf;
@@ -18,9 +19,32 @@ pub fn find_first_house_receiving(n_presents: u32) -> Option<u32> {
     None
 }
 
+pub fn find_first_house_receiving_part_2(target: u32) -> Option<u32> {
+    let mut houses: Vec<u32> = vec![0; HOUSE_LIMIT];
+    for elf in 1..HOUSE_LIMIT {
+        let mut house = elf;
+        let mut elf_count = 0;
+        while elf_count < 50 {
+            houses[house] += (elf * ELF_FACTOR2) as u32;
+            if houses[house] >= target {
+                return Some(house as u32);
+            }
+            house += elf;
+            elf_count += 1;
+            if house >= HOUSE_LIMIT {
+                break;
+            }
+        }
+    }
+    None
+}
+
 fn main() {
-    if let Some(num) = find_first_house_receiving(PART_1_NUM) {
-        println!("Part 1: House {} receives > {} presents", num, PART_1_NUM);
+    if let Some(num) = find_first_house_receiving(TARGET) {
+        println!("Part 1: House {} receives > {} presents", num, TARGET);
+    }
+    if let Some(num) = find_first_house_receiving_part_2(TARGET) {
+        println!("Part 1: House {} receives > {} presents", num, TARGET);
     }
 }
 
@@ -30,7 +54,12 @@ mod tests {
 
     #[test]
     fn test_part_1_answer() {
-        assert_eq!(find_first_house_receiving(PART_1_NUM), Some(786240));
+        assert_eq!(find_first_house_receiving(TARGET), Some(786240));
+    }
+
+    #[test]
+    fn test_part_2_answer() {
+        assert_eq!(find_first_house_receiving_part_2(TARGET), Some(831600));
     }
 
 }
