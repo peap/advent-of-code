8df18137c49223eaa2c832250fdb87dcbd9e3961
Eric Petersen <eric@huskers.unl.edu>
2016-12-20 10:04:11 -0800

    Finish 2015, Day 24, Part 1 - needed u64!!!

--- 2015-rs/day-24/src/main.rs
+++ 2015-rs/day-24/src/main.rs
@@ -6,7 +6,7 @@ use std::io::{BufRead, BufReader};
 
 use itertools::Itertools;
 
-pub type Gift = u32;
+pub type Gift = u64;
 pub type Group = Vec<Gift>;
 
 pub fn load_gifts<'a>(filename: &'a str) -> Group {
@@ -18,7 +18,6 @@ pub fn load_gifts<'a>(filename: &'a str) -> Group {
             gifts.push(text.parse().unwrap());
         }
     }
-    gifts.sort_by(|a, b| b.cmp(&a));
     gifts
 }
 
@@ -30,7 +29,7 @@ pub fn can_split_remainder(to_exclude: &Group, gifts: &Group)
             remainder.push(gift.clone());
         }
     }
-    let sum = remainder.iter().sum::<u32>();
+    let sum = remainder.iter().sum::<Gift>();
     if sum % 2 != 0 {
         return false;
     }
@@ -46,21 +45,22 @@ pub fn can_split_remainder(to_exclude: &Group, gifts: &Group)
     false
 }
 
-pub fn optimize_sleigh(gifts: Group, min: usize) -> Option<(usize, u32)> {
-    let target_weight: u32 = gifts.iter().sum::<u32>() / 3;
-    let mut lowest_qe = u32::max_value();
+pub fn optimize_sleigh(gifts: Group, min: usize) -> Option<(usize, Gift)> {
+    let target: Gift = gifts.iter().sum::<Gift>() / 3;
+    let mut lowest_qe = Gift::max_value();
     for k in min..(gifts.len() / 3) {
         let combinations = gifts.iter().combinations(k);
         for combo in combinations {
-            let combo: Group = combo.iter().map(|g| **g).collect();
-            let sum = combo.iter().fold(0, |acc, g| acc + *g);
-            if sum == target_weight && can_split_remainder(&combo, &gifts) {
-                lowest_qe = cmp::min(lowest_qe, combo.iter().fold(1, |acc, g| {
-                    acc * *g
-                }));
+            let owned_combo: Group = combo.iter().map(|g| **g).collect();
+            let sum = owned_combo.iter().fold(0, |acc, g| acc + *g);
+            if sum == target && can_split_remainder(&owned_combo, &gifts) {
+                lowest_qe = cmp::min(
+                    lowest_qe,
+                    owned_combo.iter().fold(1, |acc, g| acc * *g)
+                );
             }
         }
-        if lowest_qe < u32::max_value() {
+        if lowest_qe < Gift::max_value() {
             return Some((k, lowest_qe));
         }
     }
@@ -94,16 +94,26 @@ mod tests {
     #[test]
     fn test_example_1() {
         let items = vec![1, 2, 3, 4, 5, 7, 8, 9, 10, 11];
-        let min_qe = optimize_sleigh(items, 2);
-        assert_eq!(min_qe, Some((2, 99)));
+        let optimized = optimize_sleigh(items, 2);
+        assert_eq!(optimized, Some((2, 99)));
     }
 
-   #[test]
-   fn test_part_1() {
-       let gifts = load_gifts("input.txt");
-       if let Some((min_n, min_qe)) = optimize_sleigh(gifts, 6) {
-           assert!(min_qe > 25060087);
-       }
-   }
+    #[test]
+    fn test_someone_elses_answer() {
+        // https://www.reddit.com/r/adventofcode/comments/3y1s7f/day_24_solutions/cy9v5vo/
+        let items = vec![
+            1, 3, 5, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 67,
+            71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
+        ];
+        let optimized = optimize_sleigh(items, 6);
+        assert_eq!(optimized, Some((6, 10_439_961_859)));
+    }
+
+    #[test]
+    fn test_part_1() {
+        let gifts = load_gifts("input.txt");
+        let optimized = optimize_sleigh(gifts, 6);
+        assert_eq!(optimized, Some((6, 11846773891)));
+    }
 
 }
