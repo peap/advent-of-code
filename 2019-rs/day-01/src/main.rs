4b5daaac695b4173dc94e4937d36af058a7c24ca
Eric Petersen <eric@ericpetersen.io>
2019-12-01 13:13:53 -0800

    Day 1, Part 2, and tests

--- 2019-rs/day-01/src/main.rs
+++ 2019-rs/day-01/src/main.rs
@@ -11,8 +11,55 @@ fn fuel_for_weight(weight: &i64) -> i64 {
     (weight / 3) - 2
 }
 
+fn fuel_for_weight_recur(weight: &i64) -> i64 {
+    let mut fuel = fuel_for_weight(weight);
+    let mut additional = fuel_for_weight(&fuel);
+    while additional > 0 {
+        fuel += additional;
+        additional = fuel_for_weight(&additional);
+    }
+    fuel
+}
+
 fn main() {
     let weights = load_weights("input.txt");
-    let total = weights.iter().fold(0, |sum, w| sum + fuel_for_weight(w));
-    println!("Part 1: total weight is {}", total);
+    let total1 = weights.iter().fold(0, |sum, w| sum + fuel_for_weight(w));
+    println!("Part 1: total weight is {}", total1);
+    let total2 = weights.iter().fold(0, |sum, w| sum + fuel_for_weight_recur(w));
+    // Between 4983513 and 49883626.
+    println!("Part 2: total weight (w/fuel weight) is {}", total2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fuel_for_weight() {
+        assert_eq!(fuel_for_weight(&12), 2);
+        assert_eq!(fuel_for_weight(&14), 2);
+        assert_eq!(fuel_for_weight(&1969), 654);
+        assert_eq!(fuel_for_weight(&100756), 33583);
+    }
+
+    #[test]
+    fn test_part1() {
+        let weights = load_weights("input.txt");
+        let total1 = weights.iter().fold(0, |sum, w| sum + fuel_for_weight(w));
+        assert_eq!(total1, 3324332);
+    }
+
+    #[test]
+    fn test_fuel_for_weight_recur() {
+        assert_eq!(fuel_for_weight_recur(&14), 2);
+        assert_eq!(fuel_for_weight_recur(&1969), 966);
+        assert_eq!(fuel_for_weight_recur(&100756), 50346);
+    }
+
+    #[test]
+    fn test_part2() {
+        let weights = load_weights("input.txt");
+        let total2 = weights.iter().fold(0, |sum, w| sum + fuel_for_weight_recur(w));
+        assert_eq!(total2, 4983626);
+    }
 }
