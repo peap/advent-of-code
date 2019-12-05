f5e8070072a8820a31cd4e18e2b62369d9804ae6
Eric Petersen <eric@ericpetersen.io>
2019-12-05 00:13:16 -0800

    2019, Day 5, Part 1

--- 2019/day-05/src/main.rs
+++ 2019/day-05/src/main.rs
@@ -1,3 +1,22 @@
+use intcode::Computer;
+
+fn part1() -> i32 {
+    let mut comp = Computer::from_file("input.txt");
+    comp.set_input(1);
+    comp.execute();
+    comp.final_output().unwrap().clone()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Part 1: The final output is {}", part1());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(), 13087969);
+    }
 }
--- 2019/intcode/src/lib.rs
+++ 2019/intcode/src/lib.rs
@@ -1,55 +1,105 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[derive(Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 enum Instruction {
-  ADD,
-  MUL,
-  END,
+    ADD,
+    MUL,
+    INPUT,
+    OUTPUT,
+    END,
 }
 
-impl From<i32> for Instruction {
-    fn from(i: i32) -> Instruction {
+#[derive(Debug)]
+enum ParamMode {
+    Position,
+    Immediate,
+}
+
+impl From<i32> for ParamMode {
+    fn from(i: i32) -> ParamMode {
         match i {
-            1 => Instruction::ADD,
-            2 => Instruction::MUL,
-            99 => Instruction::END,
-            _ => panic!("Unknown instruction"),
+            0 => ParamMode::Position,
+            1 => ParamMode::Immediate,
+            _ => panic!("Unknown ParamMode {}", i),
         }
     }
 }
 
+#[derive(Debug)]
 struct Opcode {
     instruction: Instruction,
     num_params: usize,
+    param_modes: Vec<ParamMode>,
+    input: i32,
+}
+
+impl From<i32> for Opcode {
+    fn from(i: i32) -> Opcode {
+        let code = i % 100;
+        let (instruction, num_params) = match code {
+            1 => (Instruction::ADD, 3),
+            2 => (Instruction::MUL, 3),
+            3 => (Instruction::INPUT, 1),
+            4 => (Instruction::OUTPUT, 1),
+            99 => (Instruction::END, 0),
+            _ => panic!("Unknown instruction {}", code),
+        };
+        let mut remainder = i / 100;
+        let param_modes = (0..num_params)
+            .map(|_| {
+                let mode = ParamMode::from(remainder % 10);
+                remainder = remainder / 10;
+                mode
+            })
+            .collect();
+        Opcode {
+            instruction: instruction,
+            num_params: num_params,
+            param_modes: param_modes,
+            input: 0,
+        }
+    }
 }
 
 impl Opcode {
-    fn new(i: Instruction) -> Opcode {
+    fn act(&self, pos: &usize, program: &mut Program) -> Option<i32> {
         use Instruction::*;
-        let num_params = match i {
-            ADD => 3,
-            MUL => 3,
-            END => 0,
+        let mut params = vec![];
+        for n in 0..self.num_params {
+            let param_val = program[pos + 1 + n] as usize;
+            let value = match self.param_modes[n] {
+                ParamMode::Position => program[param_val],
+                ParamMode::Immediate => param_val as i32,
+            };
+            params.push(value);
+        }
+        let i = if self.num_params >= 1 {
+            program[pos + 1] as usize
+        } else {
+            0
         };
-        Opcode { instruction: i, num_params: num_params }
-    }
-
-    fn act(&self, pos: &usize, program: &mut Program) -> usize {
-        use Instruction::*;
-        let i = program[pos + 1] as usize;
-        let j = program[pos + 2] as usize;
-        let k = program[pos + 3] as usize;
+        let k = if self.num_params >= 3 {
+            program[pos + 3] as usize
+        } else {
+            0
+        };
         match self.instruction {
-          ADD => program[k] = program[i] + program[j],
-          MUL => program[k] = program[i] * program[j],
-          END => { },
+            ADD => program[k] = params[0] + params[1],
+            MUL => program[k] = params[0] * params[1],
+            INPUT => program[i] = self.input,
+            OUTPUT => return Some(params[0]),
+            END => {}
         };
-        1 + self.num_params
+        None
     }
 
     fn end(&self) -> bool {
         self.instruction == Instruction::END
+    }
+
+    fn set_input(&mut self, input: i32) {
+        self.input = input
     }
 }
 
@@ -59,6 +109,8 @@ pub struct Computer {
     program: Program,
     noun: i32,
     verb: i32,
+    input: i32,
+    outputs: Vec<i32>,
 }
 
 impl Computer {
@@ -67,6 +119,8 @@ impl Computer {
             noun: program[1].clone(),
             verb: program[2].clone(),
             program: program,
+            input: 0,
+            outputs: vec![],
         }
     }
 
@@ -74,13 +128,16 @@ impl Computer {
         let file = File::open(filename).unwrap();
         let reader = BufReader::new(file);
         Computer::new(
-            reader.split(b',').map(|c| {
-                String::from_utf8(c.unwrap())
-                    .unwrap()
-                    .trim()
-                    .parse()
-                    .unwrap()
-            }).collect()
+            reader
+                .split(b',')
+                .map(|c| {
+                    String::from_utf8(c.unwrap())
+                        .unwrap()
+                        .trim()
+                        .parse()
+                        .unwrap()
+                })
+                .collect(),
         )
     }
 
@@ -89,21 +146,32 @@ impl Computer {
         self.verb = verb;
     }
 
-    pub fn execute(&self) -> i32 {
+    pub fn set_input(&mut self, input: i32) {
+        self.input = input;
+    }
+
+    pub fn execute(&mut self) -> i32 {
         let mut program = self.program.clone();
         program[1] = self.noun;
         program[2] = self.verb;
         let mut pos = 0;
         while pos < program.len() {
-            let opcode = Opcode::new(Instruction::from(program[pos]));
+            let mut opcode = Opcode::from(program[pos]);
             if opcode.end() {
                 break;
             }
-            pos += opcode.act(&pos, &mut program);
+            opcode.set_input(self.input);
+            if let Some(output) = opcode.act(&pos, &mut program) {
+                self.outputs.push(output);
+            }
+            pos += 1 + opcode.num_params
         }
         program[0]
     }
 
+    pub fn final_output(&self) -> Option<&i32> {
+        self.outputs.last()
+    }
 }
 
 #[cfg(test)]
@@ -112,16 +180,21 @@ mod tests {
 
     #[test]
     fn test_day02_examples() {
-        let comp1 = Computer::new(vec![1, 0, 0, 0, 99]);
+        let mut comp1 = Computer::new(vec![1, 0, 0, 0, 99]);
         assert_eq!(comp1.execute(), 2);
-        let comp2 = Computer::new(vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]);
+        let mut comp2 = Computer::new(vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]);
         assert_eq!(comp2.execute(), 3500);
-        let comp3 = Computer::new(vec![2, 3, 0, 3, 99]);
+        let mut comp3 = Computer::new(vec![2, 3, 0, 3, 99]);
         assert_eq!(comp3.execute(), 2);
-        let comp4 = Computer::new(vec![2, 4, 4, 5, 99, 0]);
+        let mut comp4 = Computer::new(vec![2, 4, 4, 5, 99, 0]);
         assert_eq!(comp4.execute(), 2);
-        let comp5 = Computer::new(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]);
+        let mut comp5 = Computer::new(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]);
         assert_eq!(comp5.execute(), 30);
     }
 
+    #[test]
+    fn test_day05_examples() {
+        let mut comp = Computer::new(vec![1002, 4, 3, 4, 33]);
+        assert_eq!(comp.execute(), 1002);
+    }
 }
