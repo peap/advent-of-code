8e3da2f844c60099ce77988521cf4313d79d74a9
Eric Petersen <eric@ericpetersen.io>
2021-12-04 23:04:32 -0800

    Switch 2020 to common::InputReader and fix clippy findings

--- 2020/day-01/src/main.rs
+++ 2020/day-01/src/main.rs
@@ -1,20 +1,10 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-
-fn load_expenses(filename: &'static str) -> Vec<i64> {
-    let file = File::open(filename).unwrap();
-    let reader = BufReader::new(file);
-    reader
-        .lines()
-        .map(|l| l.unwrap().parse().unwrap())
-        .collect()
-}
+use common::InputReader;
 
 fn find_two_nums_with_sum(nums: Vec<i64>, sum: i64) -> Vec<i64> {
     for (i, first) in nums.iter().enumerate() {
         for second in nums[i + 1..nums.len()].iter() {
             if first + second == sum {
-                return vec![first.clone(), second.clone()];
+                return vec![*first, *second];
             }
         }
     }
@@ -26,7 +16,7 @@ fn find_three_nums_with_sum(nums: Vec<i64>, sum: i64) -> Vec<i64> {
         for (j, second) in nums[i + 1..nums.len()].iter().enumerate() {
             for third in nums[j + 1..nums.len()].iter() {
                 if first + second + third == sum {
-                    return vec![first.clone(), second.clone(), third.clone()];
+                    return vec![*first, *second, *third];
                 }
             }
         }
@@ -35,11 +25,12 @@ fn find_three_nums_with_sum(nums: Vec<i64>, sum: i64) -> Vec<i64> {
 }
 
 fn main() {
-    let expenses = load_expenses("input.txt");
+    let reader = InputReader::new("input.txt");
+    let expenses = reader.i64_lines();
     let nums2 = find_two_nums_with_sum(expenses.clone(), 2020);
     let product = nums2[0] * nums2[1];
     println!("Part 1: {} * {} = {}", nums2[0], nums2[1], product);
-    let nums3 = find_three_nums_with_sum(expenses.clone(), 2020);
+    let nums3 = find_three_nums_with_sum(expenses, 2020);
     let product = nums3[0] * nums3[1] * nums3[2];
     println!(
         "Part 2: {} * {} * {} = {}",
@@ -70,7 +61,8 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        let expenses = load_expenses("input.txt");
+        let reader = InputReader::new("input.txt");
+        let expenses = reader.i64_lines();
         let nums = find_two_nums_with_sum(expenses, 2020);
         assert_eq!(nums[0], 247);
         assert_eq!(nums[1], 1773);
@@ -78,7 +70,8 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        let expenses = load_expenses("input.txt");
+        let reader = InputReader::new("input.txt");
+        let expenses = reader.i64_lines();
         let nums = find_three_nums_with_sum(expenses, 2020);
         assert_eq!(nums[0], 188);
         assert_eq!(nums[1], 936);
